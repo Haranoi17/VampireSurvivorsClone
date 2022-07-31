0d73fb90310c3d40b8c64fa9ec8f361c6e1f7840
kamil <kamilszczepan1999@gmail.com>
2022-07-31 14:25:50 +0200

    test in separate files

--- src/Animations.rs
+++ src/Animations.rs
@@ -1,3 +1,5 @@
+mod Tests;
+
 use core::panic;
 use sfml::{graphics::Texture, SfBox};
 use std::{collections::HashMap, fs};
@@ -119,18 +121,4 @@ impl AnimationPlayer {
             .unwrap()
             .get_frame_count()
     }
-}
-
-#[test]
-fn test_loading_animation_from_directory() {
-    let test_animation_directory = "resources/Animations/Player/idle";
-    let animation = Animation::new(test_animation_directory);
-    println!("{:?}", animation.frames);
-}
-
-#[test]
-fn test_loading_animations_for_animation_player() {
-    let mut animation_player = AnimationPlayer::new();
-    let animations_path = String::from("resources/Animations/Player");
-    animation_player.initialize(animations_path);
 }
